4a3757d699788d8e6f62954dac2dc347a372bfd7
Dhanalakshmi M <harikrishnanmuthu2008@gmail.com>
2026-03-24 21:44:30 +0530

    Add files via upload

---
+++ waf_server.py
@@ -0,0 +1,107 @@
+from flask import Flask, request, redirect, jsonify
+import requests
+
+from ai_model import AIModel
+from firebase_logger import log_attack, get_logs
+
+app = Flask(__name__)
+
+ai = AIModel()
+
+# SERVER MACHINE IP
+SERVER = "http://192.168.0.225:5001"
+
+# CLIENT MACHINE (Live Server running here)
+CLIENT = "http://192.168.0.135:5500"
+
+ADMIN_USER = "dhana"
+ADMIN_PASS = "1234"
+
+
+@app.route("/")
+def home():
+    return "AI WAF Running"
+
+
+# USER LOGIN THROUGH WAF
+@app.route("/waf_login", methods=["POST"])
+def waf_login():
+
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    payload = username + " " + password
+
+    ip = request.remote_addr
+
+    result = ai.predict(payload)
+
+    print("Prediction:", result)
+
+
+    # NORMAL REQUEST
+    if result == "NORMAL":
+
+        try:
+
+            res = requests.post(
+                SERVER + "/login",
+                data={"username": username, "password": password}
+            )
+
+            if res.text == "LOGIN_SUCCESS":
+                return redirect(CLIENT + "/project.html")
+
+            return "Login Failed"
+
+        except:
+            return "Server Not Reachable"
+
+
+    # SQL ATTACK
+    elif result == "SQL":
+
+        log_attack(username, "SQL Injection", ip)
+
+        return "Something went wrong unable to reach the server . Try again"
+
+
+    # XSS ATTACK
+    elif result == "XSS":
+
+        log_attack(username, "XSS Attack", ip)
+
+        return "Something went wrong unable to reach the server. Try again"
+
+
+    # UNKNOWN ATTACK
+    else:
+
+        log_attack(username, "NEW ATTACK", ip)
+
+        return "Blocked : Unknown Attack"
+
+
+# ADMIN LOGIN
+@app.route("/admin_login", methods=["POST"])
+def admin_login():
+
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    if username == ADMIN_USER and password == ADMIN_PASS:
+        return redirect(CLIENT + "/admin.html")
+
+    return "Invalid Admin Login"
+
+
+# ADMIN VIEW ATTACK LOGS
+@app.route("/get_logs")
+def logs():
+
+    return jsonify(get_logs())
+
+
+if __name__ == "__main__":
+
+    app.run(host="0.0.0.0", port=5000)
